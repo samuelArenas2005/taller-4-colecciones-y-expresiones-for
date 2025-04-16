502cd5612ab9fa64242a82a03e036c79b65e75cb
SamuelArenas2005 <e.sir.samuel.arenas@cali.edu.co>
2025-04-15 20:04:56 -0500

    first commit

---
+++ build.sbt
@@ -0,0 +1,8 @@
+ThisBuild / version := "0.1.0-SNAPSHOT"
+
+ThisBuild / scalaVersion := "3.3.5"
+
+lazy val root = (project in file("."))
+  .settings(
+    name := "taller 4 fpfc colecciones y expresiones for"
+  )
---
+++ src/main/scala/Anagramas/package.scala
@@ -0,0 +1,34 @@
+package object Anagramas {
+
+  type Palabra = String
+  type Frase = List[Palabra]
+  type Ocurrencias = List[(Char, Int)]
+
+  val diccionario: List[Palabra] = List("cosas", "como", "yo", "y", "ocasos", "cayo", "mocosos", "roca", "moco", "sos")
+
+  def lOcPal(p: Palabra): Ocurrencias = {
+    List(('a', 2))
+  }
+
+  def lOcFrase(f: Frase): Ocurrencias = List(('a', 2))
+
+  lazy val diccionarioPorOcurrencias: Map[Ocurrencias, List[Palabra]] = {
+    Map()
+  }
+
+  def anagramasDePalabra(palabra: Palabra): List[Palabra] = {
+    List()
+  }
+
+  def combinaciones(locurrencias: Ocurrencias): List[Ocurrencias] = {
+    List()
+  }
+
+  def complemento(lOc: Ocurrencias, slOC: Ocurrencias): Ocurrencias = {
+    List(('a', 2))
+  }
+
+  def anagramasDeFrase(sentence: Frase): List[Frase] = {
+    List()
+  }
+}
